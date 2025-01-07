7fa3c9788a6549b41175176c5497e2665b1b019c
Satty423 <satiyajeetkumar@gmail.com>
2025-01-07 13:27:41 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 12332
+var accountPwd = "12345"
+let accountEmail = "Satyam@gmail.com"
+accountcity = "Jaipur"     
+
+// accountID = 2 //
+/* Don't use var type for variable data type due to scope issue */
+
+accountEmail = "Satty@gmail.com"
+accountPwd = "34325"
+accountcity = "Bengaluru"
+let accountState;
+
+console.table([accountID, accountEmail, accountPwd, accountcity, accountState])
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3  + 3) //code readability should be high
+
+console.log("Satyam")
+
+
+let name = "Satyam"
+let age = 18
+let isLoggedIn = false
+let state;
+let temp = null
+
+// number => 2 to the power of 53
+// bigint => big numbers
+// string => ""
+// boolean => True/false
+// null => standalone value
+// undefined => not assigned value
+// symbol => uniqueness
+
+
+
+// object
+
+console.log(typeof "null")
